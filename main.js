a443e4e3c914584b9a58be2017451bf2877adb8a
KevinGage <gageke@yahoo.com>
2015-06-26 18:15:54 -0400

    edited json to represent some actual skills. modified json to display skills

--- main.js
+++ main.js
@@ -13,6 +13,9 @@ function buildSkillsTable() {
 		$(skillItem).data(val);
 		skillItem.innerHTML = $(skillItem).data("skillName");
 		$(skillItem).addClass("skillitem");
+		$(skillItem).css("background","url(img/engram/Spear_Icon.png)");
+		$(skillItem).css("background","url(" + $(skillItem).data("image") + ")");
+		$(skillItem).css("background-size", "contain");
 		$('#body').append(skillItem);
 	});
 
