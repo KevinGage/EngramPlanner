d7fef903dd97ab630ecf7701873547cc8204b8dc
KevinGage <gageke@yahoo.com>
2015-06-26 17:41:51 -0400

    added png icon files.  edited jquery to resize divs

--- main.js
+++ main.js
@@ -2,24 +2,33 @@ var skills = {};
 
 $(document).ready(function() {
 	$.getJSON('skills.json', function(data) {
-        	skills = data;
+        skills = data;
 		buildSkillsTable();
-    	});	
+    });
 });
 
 function buildSkillsTable() {
 	$.each(skills.skills, function(key, val) {
-		var skillItem = document.createElement("div");   				
+		var skillItem = document.createElement("div");
 		$(skillItem).data(val);
-		skillItem.innerHTML = $(skillItem).data("skillName"); 
+		skillItem.innerHTML = $(skillItem).data("skillName");
 		$(skillItem).addClass("skillitem");
 		$('#body').append(skillItem);
 	});
 
-
 	$(".skillitem").hover(function() {
 		$(this).animate({opacity: '1'});
-	}, function() { 
+	}, function() {
 		$(this).animate({opacity: '0.5'});
 	});
-};
+
+	$(window).resize(function() {
+		resizeSkillWidth();
+	});
+
+	resizeSkillWidth();
+}
+
+function resizeSkillWidth() {
+	$(".skillitem").css({'height':$('.skillitem').width() + 'px'});
+}
